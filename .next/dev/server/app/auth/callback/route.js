539e1169202799fe6b4fc924f7d9b4a0821ad5ab
Brian Bensch <brian@snowschoolers.com>
2026-09-01 02:08:09 -0700

    Stop tracking build output

--- .next/dev/server/app/auth/callback/route.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/auth/callback/route.js")
-R.c("server/chunks/node_modules_next_06v1lyf._.js")
-R.c("server/chunks/node_modules_@supabase_auth-js_dist_module_00x2u05._.js")
-R.c("server/chunks/node_modules_1vdxpa6._.js")
-R.c("server/chunks/[root-of-the-server]__0q-0-yl._.js")
-R.c("server/chunks/_next-internal_server_app_auth_callback_route_actions_0gq6cy0.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/auth/callback/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/auth/callback/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
